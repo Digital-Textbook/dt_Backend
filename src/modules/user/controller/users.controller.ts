5bbbc27acffc5d532b0e926445ac8f2544a9b6d5
yuadhistrahangsubba <yuadhistrahangsubba10@gmail.com>
2024-08-21 12:17:54 +0600

    email

--- src/modules/user/controller/users.controller.ts
+++ src/modules/user/controller/users.controller.ts
@@ -17,4 +17,38 @@ export class UserController {
   async createStudent(@Body() studentData: CreateUserDto) {
     return await this.userService.createNewUser(studentData);
   }
+
+  @Post('/VerifyOtp')
+  async verifyOtp(
+    @Body() data: { phone: string; otp: string },
+  ): Promise<{ msg: string }> {
+    let prefix = '+975';
+    let phone = prefix.concat(data.phone);
+    return await this.userService.verifyOtpByPhone(phone, data.otp);
+  }
+  ////////////////////////////////////////////////////////////////////
+
+  @Post('/VerifyOtp2')
+  async verifyOtp2(
+    @Body() data: { phone: string; otp: string },
+  ): Promise<{ msg: string }> {
+    let prefix = '+975';
+    let phone = prefix.concat(data.phone);
+
+    const verificationResult = await this.userService.verifyOtpByPhone(
+      phone,
+      data.otp,
+    );
+
+    if (verificationResult.msg === 'approved') {
+      await this.userService.updateUserStatusByPhone(phone);
+    }
+
+    return verificationResult;
+  }
+
+  //   @Post('/VerifyOtpEmail')
+  //   async verifyByEmail(@Body() data: { otp: string }): Promise<void> {
+  //     await this.userService.verifyByEmail(data.otp);
+  //   }
 }
