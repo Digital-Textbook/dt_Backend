990717cd1c984f7f8a15c940d89f36a570ed6e0d
yuadhistrahangsubba <yuadhistrahangsubba10@gmail.com>
2024-08-25 21:20:06 +0600

    :lock: Admin finished

--- src/modules/admin/dto/updateAdmin.dto.ts
+++ src/modules/admin/dto/updateAdmin.dto.ts
@@ -1,14 +1,27 @@
 import { IsOptional, IsString, IsEmail, IsArray, IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateAdminDto {
+  @ApiProperty({
+    description: 'Name must be string',
+    example: 'John Doe',
+  })
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiProperty({
+    description: 'Email is required',
+    example: 'example@gmail.com',
+  })
   @IsOptional()
   @IsEmail()
   email?: string;
 
+  @ApiProperty({
+    description: 'Phone number is required',
+    example: '17543213',
+  })
   @IsOptional()
   @IsInt()
   mobile_no?: string;
@@ -21,6 +34,10 @@ export class UpdateAdminDto {
   @IsArray()
   permission?: string[];
 
+  @ApiProperty({
+    description: 'Password is required',
+    example: 'Password#76',
+  })
   @IsOptional()
   @IsString()
   password?: string;
--- src/modules/auth/dto/admin-signin.dto.ts
+++ src/modules/auth/dto/admin-signin.dto.ts
@@ -1,12 +1,18 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 import { IsEmailField } from 'src/decorators/field.decorators';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginAdminDto {
+  @ApiProperty({
+    description: 'Email is required',
+    example: 'example@gmail.com',
+  })
   @IsEmailField({
     message: 'Email address format is invalid!.',
   })
   email: string;
 
+  @ApiProperty({ description: 'Password is required', example: '12345678' })
   @IsNotEmpty({ message: 'Password is required' })
   @IsString({ message: 'Password must be a string' })
   password: string;
