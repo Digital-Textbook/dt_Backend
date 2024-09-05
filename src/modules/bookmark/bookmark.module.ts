60be290189cc897b4a0a5c4548b742e77bb39e50
yuadhistrahangsubba <yuadhistrahangsubba10@gmail.com>
2024-09-05 11:32:51 +0600

    :tada: screenTime and Bookmark 2

--- src/modules/bookmark/bookmark.module.ts
+++ src/modules/bookmark/bookmark.module.ts
@@ -6,10 +6,12 @@ import { BookmarkController } from './controller/bookmark.controller';
 import { Textbook } from '../textbook/entities/textbook.entity';
 import { Users } from '../user/entities/users.entity';
 import { ScreenTime } from './entities/screen-time.entities';
+import { ScreenTimeService } from './service/sreen-time.service';
+import { ScreenTimeController } from './controller/screen-time.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Bookmark, Textbook, Users, ScreenTime])],
-  providers: [BookmarkService],
-  controllers: [BookmarkController],
+  providers: [BookmarkService, ScreenTimeService],
+  controllers: [BookmarkController, ScreenTimeController],
 })
 export class BookmarkModule {}
