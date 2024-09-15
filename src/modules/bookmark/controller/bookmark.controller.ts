c127dd0644812079c4fdb4cd06a00804cf550f6d
yuadhistrahangsubba <yuadhistrahangsubba10@gmail.com>
2024-09-15 17:55:12 +0600

    :fire: error handling done

--- src/modules/bookmark/controller/bookmark.controller.ts
+++ src/modules/bookmark/controller/bookmark.controller.ts
@@ -6,55 +6,93 @@ import {
   Patch,
   Delete,
   Get,
+  UsePipes,
+  ValidationPipe,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 
-import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { BookmarkService } from '../service/bookmark.service';
 import { CreateBookmarkDto } from '../dto/bookmark.dto';
 
-@Controller('bookmark')
+@Controller('Digital-textbook/bookmark')
 @ApiTags('bookmark')
 export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
 
   @Post('/')
-  @ApiOkResponse({ description: 'Bookmark successfully created!' })
-  @ApiBadRequestResponse({ description: 'Bookmark cannot be created!' })
+  @ApiCreatedResponse({ description: 'Bookmark successfully created!' })
+  @ApiBadRequestResponse({ description: 'Invalid data for creating bookmark!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error while creating bookmark',
+  })
+  @ApiNotFoundResponse({ description: 'User or textbook not found!' })
   async createBookmark(@Body() data: CreateBookmarkDto) {
     return await this.bookmarkService.createBookmark(data);
   }
 
   @Patch(':id/:pageNumber')
+  @UsePipes(ValidationPipe)
   @ApiOkResponse({ description: 'Bookmark updated successfully!' })
-  @ApiBadRequestResponse({ description: 'Bookmark cannot be updated!' })
+  @ApiBadRequestResponse({ description: 'Invalid bookmark ID!' })
+  @ApiNotFoundResponse({ description: 'Bookmark not found!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error while updating bookmark details!',
+  })
   async updateBookmark(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Param('pageNumber') pageNumber: string,
   ) {
     return await this.bookmarkService.updateBookmark(id, pageNumber);
   }
 
   @Delete('/:id')
+  @UsePipes(ValidationPipe)
   @ApiOkResponse({ description: 'Bookmark deleted successfully!' })
-  @ApiBadRequestResponse({ description: 'Bookmark cannot be deleted!' })
-  async deleteBookmark(@Param('id') id: string) {
+  @ApiBadRequestResponse({ description: 'Invalid bookmark Id!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error while deleting bookmark',
+  })
+  async deleteBookmark(@Param('id', ParseUUIDPipe) id: string) {
     return await this.bookmarkService.deleteBookmark(id);
   }
 
   @Get('/:userId')
+  @UsePipes(ValidationPipe)
   @ApiOkResponse({ description: 'Bookmark of user found successfully!' })
-  @ApiBadRequestResponse({ description: 'Bookmark not found!' })
-  async getBookmarkByUserId(@Param('userId') userId: string) {
+  @ApiBadRequestResponse({ description: 'Invalid Bookmark Id!' })
+  @ApiNotFoundResponse({ description: 'Bookmark not found!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error while fetching bookmark by User ID!',
+  })
+  async getBookmarkByUserId(@Param('userId', ParseUUIDPipe) userId: string) {
     return await this.bookmarkService.getBookmarkByUserId(userId);
   }
 
   @Get('/')
+  @ApiOkResponse({ description: 'Bookmark fecthed successfully!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error while fetching bookmarks!',
+  })
   async getAllBookmark() {
     return await this.bookmarkService.getAllBookmark();
   }
 
   @Get('/:id/bookmark-details')
-  async getBookmarkById(@Param('id') id: string) {
+  @UsePipes(ValidationPipe)
+  @ApiOkResponse({ description: 'Bookmark fecthed successfully!' })
+  @ApiBadRequestResponse({ description: 'Invalid Id!' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error while fetching bookmarks!',
+  })
+  async getBookmarkById(@Param('id', ParseUUIDPipe) id: string) {
     return await this.bookmarkService.getBookmarkById(id);
   }
 }
